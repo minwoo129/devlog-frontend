ed4aa16ed53aa65ddc3aa7cf450ef34bb6069f33
minwoo129 <key0129mw@naver.com>
2024-10-09 14:14:08 +0900

    test: 레이아웃 테스트

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,12 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      screens: {
+        sm: { max: "767px" },
+        md: { min: "768px", max: "991px" },
+        lg: { min: "992px", max: "1199px" },
+        xl: { min: "1200px" },
+      },
     },
   },
   plugins: [],
