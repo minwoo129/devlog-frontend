4cc842f19b5369f66885a76bce4eff574c73a772
minwoo129 <key0129mw@naver.com>
2024-10-11 11:09:41 +0900

    feat: 작은 화면에서의 네비게이션 로고 컴포넌트 추가

---
+++ src/components/common/navigation/smallNavigation/Logo.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+  return (
+    <div className="flex justify-center items-center size-full text-3xl text-white">
+      로고
+    </div>
+  );
+}
--- src/components/common/navigation/smallNavigation/index.tsx
+++ src/components/common/navigation/smallNavigation/index.tsx
@@ -1,15 +1,16 @@
 "use client";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import { SmallNavigationProps } from "./types";
+import Logo from "./Logo";
 
 export const SmallNavigation = (args: SmallNavigationProps) => {
   const { setOpen } = args;
   return (
     <button
-      className="size-full bg-zinc-600 sm:basis-2 md:hidden lg:hidden xl:hidden"
+      className="size-full bg-zinc-800 sm:basis-2 md:hidden lg:hidden xl:hidden"
       onClick={() => setOpen(true)}
     >
-      menu
+      <Logo />
     </button>
   );
 };
