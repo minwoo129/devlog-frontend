f441cc2dd42acd3258a9e073f4cff15a16bf50fc
minwoo129 <key0129mw@naver.com>
2024-10-11 12:44:26 +0900

    fix: 왼쪽 네비게이션 영역 내부 레이아웃 수정
    1. 로고 빼고 스크롤이 가능하도록

--- src/components/common/navigation/largeNavigation/index.tsx
+++ src/components/common/navigation/largeNavigation/index.tsx
@@ -4,15 +4,16 @@ import { LargeNavigationProps } from "./types";
 export default function LargeNavigation(args: LargeNavigationProps) {
   const { setOpen } = args;
   return (
-    <div className="size-full bg-zinc-800 sm:basis-20 sm:hidden overflow-y-scroll">
+    <div className="size-full bg-zinc-800 sm:basis-20 sm:hidden">
       <Logo navigationType="large" />
-
-      <MenuBtn
-        href={"/testPage"}
-        title="테스트1"
-        onClick={() => setOpen(false)}
-        navigationType="large"
-      />
+      <div className="size-full overflow-y-scroll">
+        <MenuBtn
+          href={"/testPage"}
+          title="테스트1"
+          onClick={() => setOpen(false)}
+          navigationType="large"
+        />
+      </div>
     </div>
   );
 }
