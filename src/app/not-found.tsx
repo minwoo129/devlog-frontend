22e54709ab80d3ae994366751967fe408cd1156f
minwoo129 <key0129mw@naver.com>
2024-10-11 13:01:56 +0900

    feat: 404 에러 페이지 추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="basis-full min-h-full bg-white flex justify-center items-center">
+      <h1>존재하지 않는 페이지입니다.</h1>
+    </div>
+  );
+}
