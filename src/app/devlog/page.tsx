03aa82ccc4d760eded899b8ff258eb837bb159e2
minwoo129 <key0129mw@naver.com>
2024-10-12 16:13:42 +0900

    refactor: 타이틀 영역 수정
    1. 공통 컴포넌트로 교체

--- src/app/devlog/page.tsx
+++ src/app/devlog/page.tsx
@@ -1,9 +1,12 @@
 import PageLayer from "@/components/common/pageLayer";
+import Title from "@/components/common/title";
+import { CategoryList } from "@/components/page/blog/main";
 
 export default function Devlog() {
   return (
     <PageLayer>
-      <h1>개발 블로그</h1>
+      <Title className="mt-10 border-2 border-red-300">dev.log</Title>
+      <CategoryList />
     </PageLayer>
   );
 }
--- src/components/page/home/profile.tsx
+++ src/components/page/home/profile.tsx
@@ -2,13 +2,12 @@ import Image from "next/image";
 import { ProfileProps } from "./types";
 import Link from "next/link";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import Title from "@/components/common/title";
 
 export default function Profile(args: ProfileProps) {
   return (
     <div className="w-full h-fit mt-4 py-4 px-6 border-2 border-black rounded-xl">
-      <h1 className="text-3xl font-bold font-mono text-orange-600">
-        프론트엔드 개발자 류민우입니다.
-      </h1>
+      <Title>프론트엔드 개발자 류민우입니다.</Title>
       <Link href={"/profile"} legacyBehavior>
         <div className="flex flex-row shrink max-w-fit mt-2 rounded-lg p-2 border-2 border-gray-200 hover:bg-gray-200">
           <p>프로필 바로가기</p>
