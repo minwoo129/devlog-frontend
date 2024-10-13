453e6dae5ef7ee064576431156189b103e592623
minwoo129 <key0129mw@naver.com>
2024-10-13 14:50:59 +0900

    feat: devlog 페이지 개발
    - 메인 페이지 카테고리 리스트 컴포넌트 추가
    - 다 보여주지 않고 핵심 몇개만 보여주는거로

---
+++ src/components/page/blog/main/categoryList.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { DevLogNavDatas } from "@/components/common/navigation/navDatas";
+import { motion } from "framer-motion";
+import CategoryListItem from "./categoryListItem";
+
+export default function CategoryList() {
+  return (
+    <motion.div
+      className="flex flex-row h-fit justify-start items-center mt-10 py-3 overflow-x-scroll scrollbar-hide last:mr-0"
+      animate={{ transition: { staggerChildren: 0.1 } }}
+    >
+      {DevLogNavDatas.filter((data) => data.mainCategory).map((data, idx) => {
+        return <CategoryListItem categoryData={data} key={idx} />;
+      })}
+    </motion.div>
+  );
+}
---
+++ src/components/page/blog/main/types.ts
@@ -0,0 +1,5 @@
+import { PageNavLinkType } from "@/components/common/navigation/navDatas";
+
+export interface CategoryListItemProps {
+  categoryData: PageNavLinkType;
+}
