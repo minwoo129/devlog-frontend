c6b58a82c4a2342538c20968c7a0274b1fd52c38
minwoo129 <key0129mw@naver.com>
2024-10-14 10:45:54 +0900

    feat: 블로그 메인 카테고리 리스트 에니메이션 수정

--- src/components/page/blog/main/categoryList.tsx
+++ src/components/page/blog/main/categoryList.tsx
@@ -1,15 +1,29 @@
 "use client";
 import { DevLogNavDatas } from "@/components/common/navigation/navDatas";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 import CategoryListItem from "./categoryListItem";
 import { CategoryListProps } from "./types";
+
+const container: Variants = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+};
 
 export default function CategoryList(args: CategoryListProps) {
   const { posts } = args;
   return (
     <motion.div
       className="flex flex-row h-fit justify-start items-center mt-10 py-4 overflow-x-scroll scrollbar-hide last:mr-0"
-      animate={{ transition: { staggerChildren: 0.1 } }}
+      variants={container}
+      initial="hidden"
+      animate="show"
     >
       {DevLogNavDatas.filter((data) => data.mainCategory).map((data, idx) => {
         return <CategoryListItem categoryData={data} key={idx} />;
--- src/components/page/blog/main/categoryListItem.tsx
+++ src/components/page/blog/main/categoryListItem.tsx
@@ -1,27 +1,24 @@
 "use client";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 import { CategoryListItemProps } from "./types";
 import Link from "next/link";
+
+const item: Variants = {
+  hidden: {
+    opacity: 0,
+    x: 30,
+  },
+  show: {
+    opacity: 1,
+    x: 0,
+  },
+};
 
 export default function CategoryListItem(args: CategoryListItemProps) {
   const { categoryData } = args;
   const { title, href } = categoryData;
   return (
-    <motion.div
-      //className="flex w-28 h-40 rounded-xl bg-gray-500 mr-4"
-      initial={{
-        opacity: 0,
-        x: 30,
-      }}
-      animate={{
-        opacity: 1,
-        x: 0,
-      }}
-      transition={{
-        duration: 0.6,
-        ease: [0.6, -0.05, 0.01, 0.99],
-      }}
-    >
+    <motion.div variants={item}>
       <Link href={href}>
         <div className=" flex flex-row w-44 h-56 mr-6 shadow-lg transition ease-in-out duration-300 hover:-translate-y-2">
           <div className="w-3 h-full bg-gray-800 rounded-l-lg" />
