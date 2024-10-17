c657c025f05103351adf0d2faae531e89017946f
minwoo129 <key0129mw@naver.com>
2024-10-17 15:01:51 +0900

    feat: 컨퍼런스 전체 강의 데이터 추가
    1. 상세 페이지에서의 탐색 성능 향상을 위해

---
+++ src/components/page/conference/main/historys/index.ts
@@ -0,0 +1,6 @@
+import { ConfLectureType } from "../defDatas";
+import { GoogleIO2024_Lectures } from "./google-io/google-io-2024";
+
+export const TotalConferenceLectures: ConfLectureType[] = [
+  ...GoogleIO2024_Lectures,
+];
