dc4d48dec4e6daa58b19a325ed06a0cb1203c736
minwoo129 <key0129mw@naver.com>
2024-10-21 21:12:02 +0900

    fix: 블로그 상세 페이지 썸네일 이미지 컴포넌트 수정

--- src/components/page/blog/detail/ThumbnailImg.tsx
+++ src/components/page/blog/detail/ThumbnailImg.tsx
@@ -7,9 +7,7 @@ export default function ThumbnailImg(args: ThumbnailImgProps) {
   const { thumbnailURL } = args;
   const [isError, setError] = useState(false);
   if (thumbnailURL === "" || isError) {
-    return (
-      <div className="flex flex-row justify-center items-center mt-5 w-[960px] h-[540px] bg-slate-400" />
-    );
+    return <div className="w-full h-full max-w-[900px] max-h-[801px]" />;
   }
   return (
     <div className="w-full aspect-w-16 aspect-h-9 max-w-[900px]">
