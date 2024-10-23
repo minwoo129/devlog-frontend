bebe9ff517c9cee0c281e61d8a0a6d82c006ff2d
minwoo129 <key0129mw@naver.com>
2024-10-23 12:24:24 +0900

    test: 날짜 함수 테스트 코드 수정

--- __tests__/functions/dates.test.ts
+++ __tests__/functions/dates.test.ts
@@ -1,74 +1,20 @@
 import { calculateDateDiff } from "@/commonFunctions/dates";
-import {
-  testCases1,
-  testCases2,
-  testCases3,
-  TestValidationType1,
-} from "@/commonFunctions/dates/testCases";
-import dayjs from "dayjs";
 
 describe("dates 폴더 내 함수", () => {
-  it("임의의 두 날짜 간 차이가 정확하게 계산되는가?", () => {
-    let errorDatas: TestValidationType1[] = [];
-    for (let i = 0; i < testCases1.length; i++) {
-      const { fromDate, toDate, realDayDiff } = testCases1[i];
-      const retValue = calculateDateDiff({ fromDate, toDate });
-      if (retValue !== realDayDiff) {
-        errorDatas.push({ fromDate, toDate, realDayDiff, retValue, idx: i });
-      }
-    }
+  it("두 날짜 간 차이가 정확하게 계산되는가?", () => {
+    let fromDate = "2024-10-22";
+    let toDate = "2025-01-01";
+    let realDayDiff = 71;
 
-    if (errorDatas.length > 0) console.log("errorDatas(test1):", errorDatas);
-    expect(errorDatas.length).toBe(0);
+    const retValue = calculateDateDiff({ fromDate, toDate });
+
+    expect(retValue).toBe(realDayDiff);
   });
-  it('임의의 날짜와 시스템 날짜간 차이가 정확하게 계산되는가?(테스트날짜: 2024-10-18, 시스템날짜 => "toDate"에 연결)', () => {
-    let errorDatas: TestValidationType1[] = [];
-    for (let i = 0; i < testCases2.length; i++) {
-      const { fromDate, toDate, realDayDiff } = testCases2[i];
-      const retValue = calculateDateDiff({
-        fromDate,
-        toDate: dayjs(),
-        logEnabled: true,
-      });
-      if (retValue !== realDayDiff) {
-        errorDatas.push({ fromDate, toDate, realDayDiff, retValue, idx: i });
-      }
-    }
-
-    if (errorDatas.length > 0) console.log("errorDatas(test2):", errorDatas);
-    expect(errorDatas.length).toBe(0);
-  });
-  it('임의의 날짜와 시스템 날짜간 차이가 정확하게 계산되는가?(테스트날짜: 2024-10-18, 시스템날짜 => "fromDate"에 연결)', () => {
-    let errorDatas: TestValidationType1[] = [];
-    for (let i = 0; i < testCases3.length; i++) {
-      const { fromDate, toDate, realDayDiff } = testCases3[i];
-      const retValue = calculateDateDiff({ fromDate: dayjs(), toDate });
-      if (retValue !== realDayDiff) {
-        errorDatas.push({
-          fromDate,
-          toDate,
-          realDayDiff,
-          retValue,
-          idx: i,
-        });
-      }
-    }
-
-    console.log("errorDatas(test3):", errorDatas);
-    expect(0).toBe(0);
-  });
-  it("모든 테스트 케이스에 대해 임의의 날짜와 시스템 날짜간 차이가 정확하게 계산되는가?", () => {
-    let errorDatas: TestValidationType1[] = [];
-    for (let i = 0; i < testCases1.length; i++) {
-      const { fromDate, toDate, realDayDiff } = testCases1[i];
-      jest.useFakeTimers().setSystemTime(new Date(toDate));
-      const retValue = calculateDateDiff({ fromDate, toDate: dayjs() });
-      if (retValue !== realDayDiff) {
-        errorDatas.push({ fromDate, toDate, realDayDiff, retValue, idx: i });
-      }
-    }
-
-    if (errorDatas.length > 0) console.log("errorDatas(test4):", errorDatas);
-    expect(errorDatas.length).toBe(0);
+  it("fromDate이 toDate보다 미래일 경우에 날짜 차이값이 정확하게 계산되는가?", () => {
+    let fromDate = "2024-10-22";
+    let toDate = "2024-05-06";
+    let realDayDiff = -169;
+    const retValue = calculateDateDiff({ fromDate, toDate });
+    expect(retValue).toBe(realDayDiff);
   });
 });
