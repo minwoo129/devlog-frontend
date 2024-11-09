77dc5dd2399c7cbcc7e07ab38e262611ad06197b
minwoo129 <key0129mw@naver.com>
2024-11-09 18:53:39 +0900

    feat: Notion API 요청 로직 수정
    1. 운영인 경우 노출해야 하는 데이터만 필터링해서 반환하도록 수정

--- src/lib/notion/API/request/index.ts
+++ src/lib/notion/API/request/index.ts
@@ -1,7 +1,7 @@
 import { NotionAPIInfo } from "@/config";
 import { NotionDatabaseNames } from "../database/types";
 import { requestNotionApiArgs } from "./types";
-import NotionAPI from "../sdkFuncs";
+import NotionAPI, { getFromOneDBArgs } from "../sdkFuncs";
 
 const getDBInfo = (databaseName: NotionDatabaseNames) => {
   return NotionAPIInfo.databases[databaseName];
@@ -13,9 +13,23 @@ export const requestNotionAPI = (args: requestNotionApiArgs) => {
   // database
   const { databaseName, sorts, pageSize = 100 } = args;
   const { databaseId } = getDBInfo(databaseName);
-  return NotionAPI.getFromOneDB({
+
+  let apiArgs: getFromOneDBArgs = {
     database_id: databaseId,
     page_size: pageSize,
     sorts,
-  });
+  };
+
+  if (process.env.NODE_ENV === "production") {
+    apiArgs = {
+      ...apiArgs,
+      filter: {
+        property: "visible",
+        checkbox: {
+          equals: true,
+        },
+      },
+    };
+  }
+  return NotionAPI.getFromOneDB(apiArgs);
 };
--- src/lib/notion/API/sdkFuncs/index.ts
+++ src/lib/notion/API/sdkFuncs/index.ts
@@ -9,6 +9,8 @@ const {
   databases: { query },
 } = notion;
 
+export type getFromOneDBArgs = Parameters<typeof query>[0];
+
 const APIObj = {
   getFromOneDB: async (args: Parameters<typeof query>[0]) => query({ ...args }),
 };
