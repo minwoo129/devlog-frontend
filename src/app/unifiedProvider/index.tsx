fe44ab2112fc561bbb56cf299092600025e67803
minwoo129 <key0129mw@naver.com>
2024-11-11 16:01:10 +0900

    refactor: 공통 Provider 컴포넌트 구조 수정

--- src/app/unifiedProvider/index.tsx
+++ src/app/unifiedProvider/index.tsx
@@ -1,9 +1,22 @@
 "use client";
-import { UnifiedProviderProps } from "./types";
-//import ReactQueryProvider from "./ReactQuery";
+import React from "react";
+import { GroupedProviderType, UnifiedProviderProps } from "./types";
+import ReactQueryProvider from "./ReactQuery";
+import RecoilProvider from "./Recoil";
+
+const GroupedProvider: GroupedProviderType = ({ providers, children }) =>
+  providers.reduce(
+    (prev, context) => React.createElement(context, null, prev),
+    children
+  );
 
 export default function UnifiedProvider(args: UnifiedProviderProps) {
   const { children } = args;
   //return <ReactQueryProvider>{children}</ReactQueryProvider>;
-  return <>{children}</>;
+  //return <>{children}</>;
+  return (
+    <GroupedProvider providers={[ReactQueryProvider, RecoilProvider]}>
+      {children}
+    </GroupedProvider>
+  );
 }
--- src/app/unifiedProvider/types.ts
+++ src/app/unifiedProvider/types.ts
@@ -1,6 +1,17 @@
 export interface UnifiedProviderProps {
-  children: React.ReactNode;
+  children: JSX.Element;
 }
+
+export type GroupedProviderType = (args: GroupedProviderProps) => JSX.Element;
+type GroupedProviderProps = {
+  providers: ProviderType[];
+  children: JSX.Element;
+};
+
+export type ProviderType = (args: ProviderProps) => JSX.Element;
+type ProviderProps = {
+  children: React.ReactNode;
+};
 
 export interface ProviderCommonExtendProps {
   children: React.ReactNode;
